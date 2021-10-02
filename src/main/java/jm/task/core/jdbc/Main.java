3b9f29555593d69773e63082d8cf6e1f3704e392
Денис <anisimovets@mail.ru>
2021-10-02 18:32:34 +0300

    Init

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.ArrayList;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Иван", "Иванов", (byte) 20);
+        userService.saveUser("Петр", "Петров", (byte) 30);
+        userService.saveUser("Сидр", "Сидоров", (byte) 40);
+        userService.saveUser("Джордж", "Петров", (byte) 50);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
